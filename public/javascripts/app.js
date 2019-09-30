529ada282e2ed114ba919f50ddedf357604e4556
Kevin Abram <htwnkid@gmail.com>
2019-09-30 10:02:46 -0400

    fix

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,7 @@
 $.get('/scrape', function (res) {
+    console.log(res)
     res.forEach(function (el) {
+
         $('#root').append(`
             <div>
                 <a href="${el.link}>${el.title}</a>
