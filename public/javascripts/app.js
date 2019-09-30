ef8976fd63606c1601cc790680ff04f13da61851
Kevin Abram <htwnkid@gmail.com>
2019-09-30 09:53:38 -0400

    Deployment to Heroku and Github

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,7 @@ $.get('/scrape', function (res) {
     res.forEach(function (el) {
         $('#root').append(`
             <div>
-                <a href="${link}>${el.title}</a>
+                <a href="${el.link}>${el.title}</a>
             </div>
         `)
     })
